bcbbbda977df0e6f252d349b6b548c7cb79b89e8
tom <tomgacami@gmail.com>
2026-07-04 22:22:13 -0400

    Exercise 5.18 - step 2

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,15 +1,18 @@
-// describe('template spec', () => {
-//   it('passes', () => {
-//     cy.visit('http://example.cypress.io')
-//   })
-// })
-
 describe('Blog app', ()=>{
 
   beforeEach(()=>{
+    cy.env(['BACKEND']).then(({BACKEND})=>{
+      cy.request('POST', `${BACKEND}/testing/clean`)
 
-    cy.visit('http://localhost:5173')
-    // cy.visit('')
+      const user = {
+        name: 'Thomas Jonsonn',
+        username: 'root',
+        password: 'salainen'
+      }
+      cy.request('POST', `${BACKEND}/users`, user)
+
+    })
+   cy.visit('')
   })
 
   it('Login form is shown', ()=>{
@@ -19,4 +22,34 @@ describe('Blog app', ()=>{
     cy.get('button')
         .contains('Login')
   })
+
+  describe('Login', ()=>{
+    it('succeeds with correct credentials', ()=>{
+
+      const username = 'root'
+      const password = 'salainen'
+
+      cy.get('#input-username').type(`${username}`)
+      cy.get('#input-password').type(`${password}`)
+      cy.get('#login-button').click()
+
+      cy.contains(`${username} logged in`)
+    })
+
+    it('fails with wrong credentials', ()=>{
+      const username = 'root'
+      const password = 'wrong'
+
+      cy.get('#input-username').type(`${username}`)
+      cy.get('#input-password').type(`${password}`)
+      cy.get('#login-button').click()
+
+      cy.contains('Wrong username or password')
+          .should('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.get('.error')
+          .should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+
+  })
+
 })
